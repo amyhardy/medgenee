44bb6048ae50de39f2d7f3cf12bba6e0d482a3be
amyhardy <hello@amyhardy.com>
2024-04-29 18:17:58 -0500

    changes

--- dashboard.py
+++ dashboard.py
@@ -113,16 +113,96 @@
 # if __name__ == "__main__":
 #     main()
 import streamlit as st
+import openai
+
+secret_key = os.environ.get('OPENAI_API_KEY')
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+def query_openai(prompt, system_message, wait_time, prints_on):
+    """
+    hits OPENAI API to generate sample, waits in case of error,
+    calls sample_generator.generate to get one response
+    @param prompt: prompt to pass to the model
+    @param sample_generator: generator class
+    @param wait_time: time to wait in the event of an API request error
+    @param prints_on: whether or not to print outputs
+    @returns: text output from the model
+    """
+    # generate chatgpt response, keep trying if error received
+    while True:
+        try:
+            return generate(prompt, system_message)
+        except Exception as e:
+            if prints_on:
+                print(f'Received error from OpenAI: {e}. Pausing for {wait_time}s.')
+                st.write(f'Received error from OpenAI: {e}. Pausing for {wait_time}s.')
+                if "context_length_exceeded" in str(e):
+                    print('ChatGPT context window exceeded, skipping this sample')
+                    st.write('ChatGPT context window exceeded, skipping this sample')
+                    return None
+            time.sleep(wait_time)
+
+def generate(prompt, system_message):
+        """
+        Generates sample based on set of GPT related parameters
+        @param prompt: input prompt used
+        @param system_message: chatgpt system message
+        @returns: GPT output
+        """
+        json_format_opt = True
+        model_str = 'gpt-4-0125-preview'
+        temperature = 1.0
+        max_tokens = 1500
+        top_p = 1.0
+        frequency_penalty = 0
+        presence_penalty = 0
+
+        if json_format_opt:
+            response_format = {"type": "json_object"}
+        else:
+            response_format = None
+
+        if model_str != 'text-davinci-003':
+            sample = openai.chat.completions.create(
+                response_format=response_format,
+                model=model_str,
+                messages=[
+                    {'role': "system", 'content': system_message},
+                    {'role': "user", 'content': prompt}
+                ],
+                temperature=temperature,
+                # max_tokens=max_tokens,
+                top_p=top_p,
+                frequency_penalty=frequency_penalty,
+                presence_penalty=presence_penalty
+            )
+            return sample.choices[0].message.content
+
+        else:
+            sample = openai.completions.create(
+                model=model_str,
+                prompt=prompt,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                top_p=top_p,
+                frequency_penalty=frequency_penalty,
+                presence_penalty=presence_penalty
+            )
+            return sample.choices[0].text
 
 def main():
     st.title('Welcome to the Greeting App')
 
     # User input
-    name = st.text_input("Enter your name:", "Type your name here...")
+    topic = st.text_input("Disease, disorder, or topic here:")
 
     # Button to show greeting
-    if st.button("Greet me!"):
-        st.write(f"Hello, {name}! Nice to meet you!")
+    if st.button("Start Gene Analysis"):
+        with st.spinner(f"Generating Gene Analysis on {topic}..."):
+            answers = query_openai(input_prompt, system_message, 10, True)
 
 if __name__ == "__main__":
     main()
